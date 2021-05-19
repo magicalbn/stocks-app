523c2731c1197f0d1281b63b0b5591d27621d307
unknown <nehalvbhandari@gmail.com>
2021-05-19 18:45:36 +0530

    beta ready 0.1

--- frontend/components/UI/Advert.js
+++ frontend/components/UI/Advert.js
@@ -4,7 +4,7 @@ const Advert = () => {
     return (
         <div className="advert">
             <div className="image"></div>
-            <p className="tagline">Start Purchasing Stocks to buy your Dream Car!!!</p>
+            <p className="tagline">Start Investing now to buy your Dream Car ! !</p>
         </div>
     )
 }
